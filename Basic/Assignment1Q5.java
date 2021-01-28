da0e3cb5136eed9f008877ed86484a65852e3874
kuvsnmanikant <69764188+kuvsnmanikant@users.noreply.github.com>
2021-01-28 13:49:07 +0530

    Update Assignment1Q5.java

--- Basic/Assignment1Q5.java
+++ Basic/Assignment1Q5.java
@@ -1,33 +1,23 @@
-// finding tax amount
+import java.util.Scanner;
 
-import java.lang.*;
-import java.util.*;
+class TaxAmount{
+    public double calculateTaxAmount(int ctc){
+    	double total=0.0d;
 
+    	if(ctc>180000 && ctc<=300000){total=0.1*(ctc);}
+    	if(ctc>300000 && ctc<=500000){total=0.2*(ctc);}
+    	if(ctc>500000 && ctc<=1000000){total=0.3*(ctc);}
+    	
+    	return total;
+    }
+}
 public class Assignment1Q5 {
-    public static void main (String [] args) {
-
+    public static void main(String args[]) {
     	Scanner ss=new Scanner(System.in);
 
     	System.out.print("Enter the amount : ");
         int t=ss.nextInt();
 
-        TaxAmount obj = new TaxAmount();
-
-        System.out.print("Tax is : "+obj.calculateTaxAmount(t));
-
-    }
-}    
-
-
-class TaxAmount{
-    public double calculateTaxAmount(int ctc){
-
-    	double total=0;
-
-    	if (ctc>180000 && ctc<300001){ctc=ctc-180000; total =ctc/10;}
-    	if (ctc>300000 && ctc<500001){ctc=ctc-300000; total =12000+(ctc/5);}
-    	if (ctc>500000){ctc=ctc-500000; total =52000+((ctc/10)*3);}
-
-    	return total;
+        System.out.print("Tax is : "+new TaxAmount().calculateTaxAmount(t));
     }
 }
