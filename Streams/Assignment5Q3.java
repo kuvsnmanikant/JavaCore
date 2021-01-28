04c50aa85b83421fea7d771050f72ab6476d975f
kuvsnmanikant <69764188+kuvsnmanikant@users.noreply.github.com>
2021-01-28 16:22:06 +0530

    Update Assignment5Q3.java

--- Streams/Assignment5Q3.java
+++ Streams/Assignment5Q3.java
@@ -1,6 +1,7 @@
-
+import java.lang.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
@@ -29,35 +30,35 @@ public class Assignment5Q3 {
 	   	 }
         printUniqueCities(list).forEach(k-> System.out.println(k));
         System.out.println("--------------------------------------------");
-        tradersFromPuneSortByName(list).forEach(k-> System.out.println(k));
+        trader2sFromPuneSortByName(list).forEach(k-> System.out.println(k));
         System.out.println("--------------------------------------------");
         System.out.println(allTrader3Names(list));
         System.out.println("--------------------------------------------");
-        areAnyTradersFromIndore((ArrayList<Trader>)list).forEach(f -> System.out.println(f));
+        areAnyTrader4sFromIndore((ArrayList<Trader>)list).forEach(f -> System.out.println(f));
     }
        
        
        public static List<String> printUniqueCities (List <Trader> traders) {
-
+           List<String> a= new ArrayList<>();
            Set<String> s= traders.stream().map(p -> p.getCity()).collect(Collectors.toSet());
-           List<String> a = s.stream().collect(Collectors.toList()); 
+           a = s.stream().collect(Collectors.toList()); 
            return a;
        }
        
        
 
-       public static List<String> tradersFromPuneSortByName(List<Trader> traders) {
+       public static List<String> trader2sFromPuneSortByName(List <Trader> traders) {
     	  
-    	   List<String> l = new ArrayList<>();
-    	   traders.stream().filter(c -> c.getCity().equalsIgnoreCase("pune")).forEach(e -> l.add(e.getName()));
-    	   Collections.sort(l);
+    	   List<String> l = new ArrayList<String>();
+    	   traders.stream().filter(c -> c.getCity().equalsIgnoreCase("pune")).sorted(Comparator.comparing(e -> e.getName())).forEach(e -> l.add(e.getName()));
+    //	   Collections.sort(l);
     	   return l;
 
        }
        
        
        
-       public static String allTrader3Names(List<Trader> traders) {
+       public static String allTrader3Names(List <Trader> traders) {
     	   
     	   String a="";
     	   List<String> l = new ArrayList<>();
@@ -70,9 +71,9 @@ public class Assignment5Q3 {
        
        
        
-       public static ArrayList<Trader> areAnyTradersFromIndore(ArrayList<Trader> traders) {
+       public static ArrayList<Trader> areAnyTrader4sFromIndore(ArrayList<Trader> traders) {
     	   
-    	   ArrayList<Trader> l = new ArrayList<>();
+    	   ArrayList<Trader> l = new ArrayList<Trader>();
     	   traders.stream().filter(c -> c.getCity().equalsIgnoreCase("indore")).forEach(e -> l.add(e));
     	   return l;
        }
@@ -97,8 +98,6 @@ class Trader {
 	public String getCity() {return city;}
 	
 	
-	public String toString(){ return "Trader1{name='"+this.name+"', city='"+this.city+"'}";}
+    public String toString(){ return "Trader1{name='"+this.name+"', city='"+this.city+"'}";}
 
 }
-
-
